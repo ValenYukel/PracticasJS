1a149cb0c82e0efd4f06b6d247e679e24378bb0e
ValenYukel <vyukelson@udesa.edu.ar>
2024-05-17 12:33:12 -0300

    repo

---
+++ PracticasJS2/js/alert_condi_cuatro/index.js
@@ -0,0 +1,15 @@
+let mensaje = "Cual es tu nacionalidad?";
+let nacionalidad = prompt(mensaje);
+console.log(nacionalidad);
+
+let mensaje2 = "Cual es tu profesion?";
+let profesion = prompt(mensaje2);
+console.log(profesion);
+
+let mensaje3 = "Cuantos km caminas por dia?";
+let kms = prompt(mensaje3);
+console.log(kms);
+
+function filosofoHipster(){
+    
+ }
